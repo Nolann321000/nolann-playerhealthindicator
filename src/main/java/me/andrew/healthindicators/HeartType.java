a3968ad386f6a49d94b705dfb97f5cb5f1842313
Nolann <nolann.grascoeur.ng@gmail.com>
2026-07-19 12:47:45 +0200

    Update HeartType.java

--- src/main/java/me/andrew/healthindicators/HeartType.java
+++ src/main/java/me/andrew/healthindicators/HeartType.java
@@ -7,11 +7,33 @@ public enum HeartType {
     RED_FULL(Identifier.withDefaultNamespace("hud/heart/full")),
     RED_HALF(Identifier.withDefaultNamespace("hud/heart/half")),
     YELLOW_FULL(Identifier.withDefaultNamespace("hud/heart/absorbing_full")),
-    YELLOW_HALF(Identifier.withDefaultNamespace("hud/heart/absorbing_half"));
+    YELLOW_HALF(Identifier.withDefaultNamespace("hud/heart/absorbing_half")),
+    POISONED_FULL(Identifier.withDefaultNamespace("hud/heart/poisoned_full")),
+    POISONED_HALF(Identifier.withDefaultNamespace("hud/heart/poisoned_half")),
+    WITHERED_FULL(Identifier.withDefaultNamespace("hud/heart/withered_full")),
+    WITHERED_HALF(Identifier.withDefaultNamespace("hud/heart/withered_half"));
 
     public final Identifier texture;
 
     HeartType(Identifier texture) {
         this.texture = texture;
+    }
+
+    public static HeartType applyStatus(HeartType baseType, boolean poisoned, boolean withered) {
+        if (withered) {
+            return switch (baseType) {
+                case RED_FULL, YELLOW_FULL -> WITHERED_FULL;
+                case RED_HALF, YELLOW_HALF -> WITHERED_HALF;
+                default -> baseType;
+            };
+        }
+        if (poisoned) {
+            return switch (baseType) {
+                case RED_FULL, YELLOW_FULL -> POISONED_FULL;
+                case RED_HALF, YELLOW_HALF -> POISONED_HALF;
+                default -> baseType;
+            };
+        }
+        return baseType;
     }
 }
